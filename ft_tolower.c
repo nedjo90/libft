31c3b5d9e683f1f6a0cc6c5487e83370e9c6a5a3
Necati Han <han.necati@gmail.com>
2023-07-10 14:47:05 +0200

    update

--- ft_tolower.c
+++ ft_tolower.c
@@ -5,32 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: necatihan <necatihan@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/10 11:33:34 by nhan              #+#    #+#             */
-/*   Updated: 2023/07/10 14:29:03 by necatihan        ###   ########.fr       */
+/*   Created: 2023/07/10 14:33:30 by necatihan         #+#    #+#             */
+/*   Updated: 2023/07/10 14:45:16 by necatihan        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-int ft_tolower(int c)
+int	ft_tolower(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
 		c = c + 97 - 65;
 	}
 	return (c);
-}
-
-int main(int argc, char **argv)
-{
-	int i;
-
-	(void)argc;
-	i = 0;
-	while (argv[1][i])
-	{
-		argv[1][i] = ft_tolower(argv[1][i]);
-		write(1, &argv[1][i], 1);
-		i++;
-	}
-	return (0);
 }
--- main.c
+++ main.c
@@ -1,12 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: necatihan <necatihan@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/10 14:20:27 by necatihan         #+#    #+#             */
-/*   Updated: 2023/07/10 14:21:31 by necatihan        ###   ########.fr       */
+/*   Created: 2023/07/10 14:43:23 by necatihan         #+#    #+#             */
+/*   Updated: 2023/07/10 14:46:50 by necatihan        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_tolower(int c);
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	(void)argc;
+	i = 0;
+	while (argv[1][i])
+	{
+		argv[1][i] = ft_tolower(argv[1][i]);
+		write(1, &argv[1][i], 1);
+		i++;
+	}
+	return (0);
+}
