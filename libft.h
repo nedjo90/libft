b2c4a5c0b1db935ea1ea925ec94ff5f98ededbfa
Necati Han <han.necati@gmail.com>
2023-07-10 14:29:52 +0200

    update

--- libft.h
+++ libft.h
@@ -1,36 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: necatihan <necatihan@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/10 11:33:34 by nhan              #+#    #+#             */
-/*   Updated: 2023/07/10 14:29:03 by necatihan        ###   ########.fr       */
+/*   Created: 2023/07/10 14:20:27 by necatihan         #+#    #+#             */
+/*   Updated: 2023/07/10 14:21:31 by necatihan        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-int ft_tolower(int c)
-{
-	if (c >= 65 && c <= 90)
-	{
-		c = c + 97 - 65;
-	}
-	return (c);
-}
-
-int main(int argc, char **argv)
-{
-	int i;
-
-	(void)argc;
-	i = 0;
-	while (argv[1][i])
-	{
-		argv[1][i] = ft_tolower(argv[1][i]);
-		write(1, &argv[1][i], 1);
-		i++;
-	}
-	return (0);
-}
