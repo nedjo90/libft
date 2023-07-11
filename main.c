f2cecaed29b91a31dd98d31ad906ecc9fbafcfff
Necati Han <han.necati@gmail.com>
2023-07-11 10:50:48 +0200

    update

--- main.c
+++ main.c
@@ -6,11 +6,14 @@
 /*   By: necatihan <necatihan@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 14:53:42 by necatihan         #+#    #+#             */
-/*   Updated: 2023/07/11 00:14:47 by necatihan        ###   ########.fr       */
+/*   Updated: 2023/07/11 10:43:55 by necatihan        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int	ft_tolower(int c);
 int	ft_toupper(int c);
@@ -19,17 +22,17 @@ int	ft_isascii(int c);
 int	ft_isalnum(int c);
 int	ft_isdigit(int c);
 int	ft_isalpha(int c);
+int	ft_atoi(char *str);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
 int	main(int argc, char **argv)
 {
 	int	i;
+	int	j;
 
 	(void)argc;
-	i = 0;
-	while (ft_isprint(argv[1][i]) != 0)
-	{
-		write(1, &argv[1][i], 1);
-		i++;
-	}
+	i = strncmp(argv[1], argv[2], atoi(argv[3]));
+	j = ft_strncmp(argv[1], argv[2], atoi(argv[3]));
+	printf("strncmp  = %d\nft_strncmp = %d", i, j);
 	return (0);
 }
