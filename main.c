640ba93d29f24dcc81da189b26b2311ff10d6f02
Necati Han <han.necati@gmail.com>
2023-07-11 13:23:19 +0200

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: necatihan <necatihan@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 14:53:42 by necatihan         #+#    #+#             */
-/*   Updated: 2023/07/11 12:59:03 by necatihan        ###   ########.fr       */
+/*   Updated: 2023/07/11 13:22:15 by necatihan        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,13 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strnstr(const char *haystack, char *needle, size_t len);
 char	*ft_strstr(const char *haystack, const char *needle);
+char	*ft_strrchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
 
 int	main(int argc, char **argv)
 {
 	(void)argc;
-	printf("strstr  = %s\nft_strstr = %s", strstr(argv[1], argv[2]), 
-		ft_strstr(argv[1], argv[2]));
+	printf("strrchr  = %s\nft_strrchr = %s", strchr(argv[1], (int)(argv[2][0])), 
+		ft_strchr(argv[1], (int)(argv[2][0])));
 	return (0);
 }
