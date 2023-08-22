72a77bd63369e4a67a2d1564f6344ab42c09f8f3
Necati Han <han.necati@gmail.com>
2023-08-22 17:36:56 +0200

    update

--- ft_strndup.c
+++ ft_strndup.c
@@ -1,25 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: necatihan <necatihan@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/22 17:20:18 by necatihan         #+#    #+#             */
-/*   Updated: 2023/08/22 17:30:26 by necatihan        ###   ########.fr       */
+/*   Created: 2023/08/22 17:27:27 by necatihan         #+#    #+#             */
+/*   Updated: 2023/08/22 17:35:10 by necatihan        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s1)
+char *ft_strndup(const char *s1, size_t n)
 {
 	size_t	i;
 	char	*str;
 
 	i = 0;
 	while (s1[i] != '\0')
+	{
+		if (i == n)
+		{
+			str = (char *) malloc ((i) * sizeof (char));
+			return (str);
+		}
 		i++;
-	str = (char *) malloc((i + 1) * sizeof (char));
-	return (str);
+	}
+	if (i + 1 <= n)
+	{
+		str = (char *) malloc ((i + 1) * sizeof (char));
+		return (str);
+	}
+	else
+	{
+		str = (char *) malloc ((i) * sizeof (char));
+		return (str);
+	}
 }
